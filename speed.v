35bcd25f82a65bb4aaff2dfe2b383e3ee7d66fe5
meganpashkja <152100531+meganpashkja@users.noreply.github.com>
2024-11-12 01:40:12 -0500

    Update speed.v

--- speed.v
+++ speed.v
@@ -23,7 +23,7 @@ module half_second(Clock, Resetn, HEnable, QEnable, EEnable, HSecEn);
             count <= count + 1'b1;
 
             if (count == 26'b1011111010111100001000000) begin           // 25000000
-                count <= 2'b00;
+                count <= 26'b00;
                 HSecEn <= 1'b1;
             end
             else
@@ -40,14 +40,14 @@ module quarter_second(Clock, Resetn, HEnable, QEnable, EEnable, QSecEn);
    always @ (posedge Clock) 
    begin
        if (Resetn == 0) begin
-            count <= 2'b00;
+            count <= 26'b00;
             QSecEn <= 1'b0;
         end
         else if (!HEnable && QEnable && !EEnable) begin
             count <= count + 1'b1;
 
             if (count == 26'b101111101011110000100000) begin            // 12500000
-                count <= 2'b00;
+                count <= 26'b00;
                 QSecEn <= 1'b1;
             end
             else
@@ -64,14 +64,14 @@ module eigth_second(Clock, Resetn, HEnable, QEnable, EEnable, ESecEn);
    always @ (posedge Clock) 
    begin
        if (Resetn == 0) begin
-            count <= 2'b00;
+            count <= 26'b00;
             ESecEn <= 1'b0;
         end
         else if (!HEnable && !QEnable && EEnable) begin
             count <= count + 1'b1;
 
             if (count == 26'b10111110101111000010000) begin            // 6250000
-                count <= 2'b00;
+                count <= 26'b00;
                 ESecEn <= 1'b1;
             end
             else
