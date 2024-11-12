aa553bd648798f95a526779627dca702260e695c
meganpashkja <152100531+meganpashkja@users.noreply.github.com>
2024-11-12 01:36:18 -0500

    Add files via upload

--- keyboard.v
+++ keyboard.v
@@ -1,4 +1,4 @@
-module keyboard(CLOCK_50, Reset, received_data, EnterEn, LeftEn, RightEn, received_data_en, key_data);
+module keyboard(CLOCK_50, Reset, EnterEn, LeftEn, RightEn);
     // make code -> when key is pressed
 
     // enter make code: 5A
@@ -8,16 +8,13 @@ module keyboard(CLOCK_50, Reset, received_data, EnterEn, LeftEn, RightEn, receiv
     // right arrow key make code: E0,74
     // right arrow key break code: E0,F0,74
 
-    input CLOCK_50, Reset, received_data_en;
-    input [7:0] received_data;
+    input CLOCK_50, Reset;
     output reg EnterEn, LeftEn, RightEn;    // key enable signals
-    output reg [7:0] key_data;
 
 
-    // Commented out for simulation purposes:
-    //wire [7:0] received_data;
-    //reg [7:0] key_data;
-    //wire received_data_en;
+    wire [7:0] received_data;
+    reg [7:0] key_data;
+    wire received_data_en;
 
 
     // Unused
@@ -25,11 +22,11 @@ module keyboard(CLOCK_50, Reset, received_data, EnterEn, LeftEn, RightEn, receiv
     wire send_command, PS2_CLK, PS2_DAT, command_was_sent, error_communication_timed_out;
 
 
-    // Commented out for simulation purposes:
-    // PS2_Controller keyb(CLOCK_50, Reset, the_command, send_command, PS2_CLK, PS2_DAT, command_was_sent, error_communication_timed_out, received_data, received_data_en);
+    PS2_Controller keyb(CLOCK_50, Reset, the_command, send_command, PS2_CLK, PS2_DAT, command_was_sent, error_communication_timed_out, received_data, received_data_en);
 
 
     // shift register to load in received_data bit when received_en is high
+    // shift reg in Altera_UP_PS2_Data_In.v loads val into received_data[0] only
     always @(posedge CLOCK_50) begin
         if (Reset)
             key_data <= 8'h0;
@@ -39,45 +36,45 @@ module keyboard(CLOCK_50, Reset, received_data, EnterEn, LeftEn, RightEn, receiv
     end
 
     // State machine to detect make and break codes
-    parameter ENTER = 8'h5A, LEFT = 8'h6B, RIGHT = 8'h74, EXTENDED = 8'hE0, BREAK = 8'hF0,      	// key codes
-                IDLE = 2'b00, KEY_PRESSED = 2'b01, EXT_MAKE_CODE = 2'b10, BREAK_CODE = 2'b11;           // state codes
+    parameter ENTER = 8'h5A, LEFT = 8'h6B, RIGHT = 8'h74, EXTENDED = 8'hE0, BREAK = 8'hF0,      // key codes
+                IDLE = 2'b00, KEY_PRESSED = 2'b01, EXT_MAKE_CODE = 2'b10, BREAK_CODE = 2'b11;                        // state codes
 
-    reg [1:0] y, Y;     		// current and next state
-    reg enter_en, left_en, right_en;
+    reg [1:0] y, Y;     // current and next state
+	reg enter_en, left_en, right_en;
 
     always @(*) begin
         case (y)
         IDLE:   begin
                     if (received_data_en)
-			Y = KEY_PRESSED;
+						Y = KEY_PRESSED;
                     else
                         Y = IDLE;
                 end
-	KEY_PRESSED:	begin
-			if (key_data == ENTER) begin             // key_data = 5A
-				enter_en = 1'b1;
-				left_en = 1'b0;
-				right_en = 1'b0;
-				Y = IDLE;
-			end
-			else if (key_data == EXTENDED)
-				Y = EXT_MAKE_CODE;
-			else if (key_data == BREAK)
-				Y = BREAK_CODE;
-			else
-				Y = IDLE;
-			end
+		KEY_PRESSED:	begin
+							if (key_data == ENTER) begin             // key_data = 5A
+							enter_en = 1'b1;
+							left_en = 1'b0;
+							right_en = 1'b0;
+							Y = IDLE;
+						end
+						else if (key_data == EXTENDED)
+							Y = EXT_MAKE_CODE;
+						else if (key_data == BREAK)
+							Y = BREAK_CODE;
+						else
+							Y = IDLE;
+						end
         EXT_MAKE_CODE:  begin
-			    if (key_data == LEFT) begin              // key_data = E0,6B
-				enter_en = 1'b0;
-				left_en = 1'b1;
-				right_en = 1'b0;
-				Y = IDLE;
+                            if (key_data == LEFT) begin              // key_data = E0,6B
+                                enter_en = 1'b0;
+								left_en = 1'b1;
+								right_en = 1'b0;
+                                Y = IDLE;
                             end
                             else if (key_data == RIGHT) begin        // key_data = E0,74
                                 enter_en = 1'b0;
-				left_en = 1'b0;
-				right_en = 1'b1;
+								left_en = 1'b0;
+								right_en = 1'b1;
                                 Y = IDLE;
                             end
                             else if (key_data == BREAK)
@@ -88,19 +85,19 @@ module keyboard(CLOCK_50, Reset, received_data, EnterEn, LeftEn, RightEn, receiv
         BREAK_CODE: begin
                         if (key_data == ENTER) begin
                             enter_en = 1'b0;
-			    left_en = 1'b0;
-			    right_en = 1'b0;
+							left_en = 1'b0;
+							right_en = 1'b0;
                             Y = IDLE;
                         end
                         else if (key_data == LEFT) begin
-			    enter_en = 1'b0;
+							enter_en = 1'b0;
                             left_en = 1'b0;
-			    right_en = 1'b0;
+							right_en = 1'b0;
                             Y = IDLE;
                         end
                         else if (key_data == RIGHT) begin
-			    enter_en = 1'b0;
-			    left_en = 1'b0;
+							enter_en = 1'b0;
+							left_en = 1'b0;
                             right_en = 1'b0;
                             Y = IDLE;
                         end
@@ -119,13 +116,13 @@ module keyboard(CLOCK_50, Reset, received_data, EnterEn, LeftEn, RightEn, receiv
     always @(posedge CLOCK_50) begin
         if (Reset) begin
             y <= IDLE;
-	    EnterEn <= 1'b0;
+			EnterEn <= 1'b0;
             LeftEn <= 1'b0;
             RightEn <= 1'b0;
 		end
         else
             y <= Y;
-	    EnterEn <= enter_en;
+			EnterEn <= enter_en;
             LeftEn <= left_en;
             RightEn <= right_en;
     end
---
+++ speed.v
@@ -0,0 +1,81 @@
+module speed(CLOCK_50, SW, LEDR);
+    input CLOCK_50;
+    input [3:0] SW;
+    output [2:0] LEDR;
+
+    half_second h1(CLOCK_50, SW[3], SW[2], SW[1], SW[0], LEDR[2]);
+    quarter_second q1(CLOCK_50, SW[3], SW[2], SW[1], SW[0], LEDR[1]);
+    eigth_second e1(CLOCK_50, SW[3], SW[2], SW[1], SW[0], LEDR[0]);
+endmodule
+
+module half_second(Clock, Resetn, HEnable, QEnable, EEnable, HSecEn);
+    input Clock, Resetn, HEnable, QEnable, EEnable;
+    output reg HSecEn;
+    reg [25:0] count;
+
+    always @(posedge Clock)
+    begin
+        if (Resetn == 0) begin
+            count <= 26'b0;
+            HSecEn <= 1'b0;
+        end
+        else if (HEnable && !QEnable && !EEnable) begin
+            count <= count + 1'b1;
+
+            if (count == 26'b1011111010111100001000000) begin           // 25000000
+                count <= 2'b00;
+                HSecEn <= 1'b1;
+            end
+            else
+                HSecEn <= 1'b0;
+        end
+    end
+endmodule
+
+module quarter_second(Clock, Resetn, HEnable, QEnable, EEnable, QSecEn);
+   input Clock, Resetn, HEnable, QEnable, EEnable;
+   output reg QSecEn;
+   reg [25:0] count;
+
+   always @ (posedge Clock) 
+   begin
+       if (Resetn == 0) begin
+            count <= 2'b00;
+            QSecEn <= 1'b0;
+        end
+        else if (!HEnable && QEnable && !EEnable) begin
+            count <= count + 1'b1;
+
+            if (count == 26'b101111101011110000100000) begin            // 12500000
+                count <= 2'b00;
+                QSecEn <= 1'b1;
+            end
+            else
+                QSecEn <= 1'b0;
+        end
+   end
+endmodule
+
+module eigth_second(Clock, Resetn, HEnable, QEnable, EEnable, ESecEn);
+   input Clock, Resetn, HEnable, QEnable, EEnable;
+   output reg ESecEn;
+   reg [25:0] count;
+
+   always @ (posedge Clock) 
+   begin
+       if (Resetn == 0) begin
+            count <= 2'b00;
+            ESecEn <= 1'b0;
+        end
+        else if (!HEnable && !QEnable && EEnable) begin
+            count <= count + 1'b1;
+
+            if (count == 26'b10111110101111000010000) begin            // 6250000
+                count <= 2'b00;
+                ESecEn <= 1'b1;
+            end
+            else
+                ESecEn <= 1'b0;
+        end
+   end
+endmodule
