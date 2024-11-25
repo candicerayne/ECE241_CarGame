e27da3e5beb237ec3e6f62685e2cbfb1c350267a
meganpashkja <152100531+meganpashkja@users.noreply.github.com>
2024-11-25 17:46:11 -0500

    Update hit_detector.v

--- hit_detector.v
+++ hit_detector.v
@@ -1,8 +1,7 @@
-module hit_detector(CLOCK_50, EnterEn, x_car, y_car, x_po, y_po, x_coin, y_coin, CoinEn, PoliceEn);
+module hit_detector(CLOCK_50, EnterEn, x_car, x_po, y_po, x_coin, y_coin, CoinEn, PoliceEn);
     input CLOCK_50, EnterEn;
-    input [4:0] x_car, y_car, x_po, y_po;
-    input x_coin, y_coin;
-    output [2:0] coin_count;
+    input [4:0] x_car, x_po, y_po;
+    input [2:0] x_coin, y_coin;
     output reg CoinEn, PoliceEn;
 
     parameter IDLE = 2'b00, NO_HIT = 2'b01, COIN = 2'b10, POLICE = 2'b11;
